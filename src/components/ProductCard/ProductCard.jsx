fbb31c0e678758acd0979c963f9b1ddbc31e2dcc
Igor Carvalho <igorcarvalho@aridesa.com.br>
2023-08-30 07:55:31 -0300

    Adicionando display grid aos itens e loading antes de carregar os itens

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -3,6 +3,7 @@ import propTypes from 'prop-types';
 import { BsFillCartPlusFill } from 'react-icons/bs';
 
 import './ProductCard.css';
+import formatCurrency from '../../utils/formatCurrency';
 
 function ProductCard({ data }){
   const { title, thumbnail, price } = data;
@@ -10,10 +11,10 @@ function ProductCard({ data }){
   return(
     <section className="product_card">
 
-      <img src={thumbnail} alt="product" className="card_image" />
+      <img src={thumbnail.replace(/\w\.jpg/gi, 'W.jpg')} alt="product" className="card_image" />
       
       <div className="card_infos">
-        <h2 className="card_price">{price}</h2>
+        <h2 className="card_price">{formatCurrency(price/100, 'BRL')}</h2>
         <h2 className="card_title">{title}</h2>
       </div>
       
